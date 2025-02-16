add4cf623a320911762fff591de8f722bced2afc
Miguel Torres <miguel.torres@gmail.com>
2025-02-16 12:27:45 +0000

    WIP split code into some separated source and header files

---
+++ ESP32_sketches/PlatformIO sketches/PP-motorized-injector/src/injector_fsm.cpp
@@ -0,0 +1,345 @@
+#include "config.h"
+#include "injector_fsm.h"
+
+boolean readNozzleTemp = true;
+int nozzleTemperature;
+
+boolean readEmergencyStop = true;
+boolean emergencyStop;
+
+boolean readSelectButton = true;
+boolean selectButtonPressed;
+
+boolean readUpButton = true;
+boolean upButtonPressed;
+
+boolean readDownButton = true;
+boolean downButtonPressed;
+
+boolean readTopEndStop = true;
+boolean topEndStopActivated;
+
+boolean readBottomEndStop = true;
+boolean bottomEndStopActivated;
+
+boolean readBarrelEndStop = true;
+boolean barrelEndStopActivated;
+
+
+InjectorStates currentState = InjectorStates::INIT_HEATING;  // declaring variable runState can only have valid values of enum
+uint16_t error = InjectorError::NO_ERROR;
+
+////////////////////
+/**
+ * should run in each loop, or at least every 10ms, and if any endstop or emergency stop is triggered, stop machine
+ * and report which endstop has been triggered... possible thereafter only allow moves in opposite direction, or back to refill, or
+ * other options..?
+ *
+ * @returns InjectorError
+ */
+int sanityCheck()
+{
+  int error = InjectorError::NO_ERROR;
+
+  if (nozzleTemperature <= minTempForAnyMove) // and stop machine
+  {
+    error |= InjectorError::ERROR_1;
+  }
+
+  if (barrelEndStopActivated)
+  {
+    error |= InjectorError::ERROR_2;
+  }
+
+  if (topEndStopActivated)
+  {
+    error |= InjectorError::ERROR_3;
+  }
+
+  if (bottomEndStopActivated)
+  {
+    error |= InjectorError::ERROR_4;
+  }
+
+  if (emergencyStop)
+  {
+    error |= InjectorError::ERROR_5;
+  }
+  // FIXME add error state when function REQUIRES something under barrel (compression, purge or mould fill)
+  return error;
+}
+/**
+ * to transition states would also apply button activation / availablity..? or this only in machine states..?
+ * some motor moves user may want to stop before motor move is completed..? INJECT or HOLD, or COMPRESSION
+ * how to apply ContinuousMotorMoves..? As in Purge, where State does not change whilst moving motor.. this function must be in Machine State..?
+ *
+ */
+void transitionToState(InjectorStates toState)
+{
+  if (currentState != toState)
+  {
+    Serial.printf("Changing from %d to %d\n", currentState, toState);
+
+    if (toState == ERROR_STATE)
+    {
+      stepper->stopMove();
+    }
+
+    else if (toState == INIT_HOMED_ENCODER_ZEROED)
+    {
+      doMoveMotor = true;
+      continuousMotorMoveUp(generalFastSpeed); // FIXME this should be...: ?
+      // homeMove(generalFastSpeed, homingSlowSpeed);  // followed by
+      // encoder.setCount(0);  // or this last line should go in machineState..? reset has to occur AFTER homeMove finishes, but not PART of homeMove
+      // transitionToState(InjectorStates::REFILL);  and finally transition to next State
+    }
+    else if (toState == REFILL) // once INIT_HOMED_ENCODER_ZEROED done, REFILL is where plunger at homed offset for plastic filling
+    {
+      doMoveMotor = true;
+      programmedMotorMove(generalFastSpeed, refillOpeningOffsetDistSteps /*,defaultAcceletationNema*/);
+    }
+    else if (toState == COMPRESSION)
+    {
+      // doMoveMotor = true;
+      // compressionFunction();  // FIXME  compression function currently commented out
+    }
+    else if (toState == INJECT)
+    {
+      doMoveMotor = true;
+      programmedMotorMove(fillMouldMoveSpeed, fillMouldMoveDistSteps, fillMouldAccel);
+    }
+    else if (toState == HOLD_INJECTION)
+    {
+      doMoveMotor = true;
+      programmedMotorMove(holdMouldMoveSpeed, holdMouldMoveDistSteps /*, fillMouldAccel*/); // leave as default or as same as fillMouldAccel?
+    }
+    else if (toState == RELEASE)
+    {
+      doMoveMotor = true;
+      programmedMotorMove(releaseMouldMoveSpeed, releaseMouldMoveDistSteps); // common machine action, uses default accel
+    }
+    else
+    {
+      switch (currentState)
+      {
+
+      case INIT_HOMED_ENCODER_ZEROED:
+
+        break;
+      }
+    }
+
+    currentState = toState;
+  }
+}
+
+void machineState() //
+{
+  // static StatesMachine runState;  // useful here, or to make runState a Static variable between loops
+  //  maybe can add "static" to above enum declaration...?
+
+  /** button activation/ availability is activted on entering new state..? if motor move is started during transtion, STOPPING move by user
+   * can only be done once in new State..?
+   */
+
+  switch (currentState)
+  {
+  case InjectorStates::ERROR_STATE: // function that includes stopMove(), flashes LEDs red (maybe with
+    // Serial.println("ERROR!! " + error);
+
+    buttonLEDsColors(RED_RGB, RED_RGB, RED_RGB); // FIXME ERROR_STATE should be all red, with flashing sequence as per Error to Identify
+
+    if (selectButtonPressed) // FIXME ERROR_STATE this is only for testing, SHOULD NOT EXIST IN REAL SKETCH
+    {
+      transitionToState(InjectorStates::INIT_HEATING);
+    }
+    break;
+
+  case InjectorStates::INIT_HEATING: //  FIXME INIT_HEATING this is only for testing, SHOULD NOT EXIST IN REAL SKETCH
+    buttonLEDsColors(RED_RGB, RED_RGB, RED_RGB);
+
+    if (upButtonPressed)
+    {
+      transitionToState(InjectorStates::INIT_HOT_NOT_HOMED);
+    }
+
+    if (nozzleTemperature > minTempForAnyMove)
+    {
+      transitionToState(InjectorStates::INIT_HOT_NOT_HOMED);
+    }
+
+    break;
+
+  case InjectorStates::INIT_HOT_NOT_HOMED:
+    buttonLEDsColors(YELLOW_RGB, YELLOW_RGB, YELLOW_RGB);
+
+    if (upButtonPressed)
+    {
+      transitionToState(InjectorStates::INIT_HOMED_ENCODER_ZEROED);
+    }
+    if (downButtonPressed) //  FIXME INIT_HOT_NOT_HOME downButtonPressed this is only for testing, SHOULD NOT EXIST IN REAL SKETCH
+    {
+      transitionToState(InjectorStates::INIT_HEATING);
+    }
+    break;
+
+  case InjectorStates::INIT_HOMED_ENCODER_ZEROED:      // FIXME should be only the reset of the encoder, no button presses exist in real sketch
+    buttonLEDsColors(YELLOW_RGB, RED_RGB, YELLOW_RGB); //  FIXME YELLOW_RGB, YELLOW_RGB, YELLOW_RGB
+
+    if (downButtonPressed)
+    {
+      transitionToState(InjectorStates::REFILL);
+    }
+    if (upButtonPressed)
+    {
+      transitionToState(InjectorStates::INIT_HOT_NOT_HOMED);
+    }
+    break;
+
+  case InjectorStates::REFILL:
+    if (endOfDayFlag == 0)
+    {
+      buttonLEDsColors(GREEN_RGB, BLACK_RGB, BLACK_RGB);
+    }
+    else if (endOfDayFlag == 1)
+    {
+      buttonLEDsColors(GREEN_RGB, BLUE_RGB, BLUE_RGB);
+    }
+
+    if (selectButtonPressed)
+    {
+      transitionToState(InjectorStates::COMPRESSION);
+    }
+
+    if (upButtonPressed && downButtonPressed)
+    {
+      endOfDayFlag = !endOfDayFlag;
+    }
+
+    break;
+
+  case InjectorStates::COMPRESSION:
+    buttonLEDsColors(RED_RGB, BLACK_RGB, RED_RGB);
+
+    if (selectButtonPressed)
+    {
+      transitionToState(InjectorStates::REFILL);
+    }
+    break;
+
+  case InjectorStates::READY_TO_INJECT:
+    buttonLEDsColors(GREEN_RGB, YELLOW_RGB, GREEN_RGB);
+
+    if (selectButtonPressed && downButtonPressed)
+    {
+      transitionToState(InjectorStates::PURGE_ZERO);
+    }
+    else if (upButtonPressed)
+    {
+      transitionToState(InjectorStates::REFILL);
+    }
+
+    break;
+
+  case InjectorStates::PURGE_ZERO:
+    buttonLEDsColors(GREEN_RGB, YELLOW_RGB, YELLOW_RGB);
+    if (selectButtonPressed)
+    {
+      stepper->setCurrentPosition(0);
+      transitionToState(InjectorStates::ANTIDRIP);
+    }
+
+    if (upButtonPressed)
+    {
+      doMoveMotor = true;
+      continuousMotorMoveUp(purgeSpeed);
+    }
+
+    if (downButtonPressed)
+    {
+      doMoveMotor = true;
+      continuousMotorMoveDown(purgeSpeed);
+    }
+
+    break;
+
+  case InjectorStates::ANTIDRIP:
+    buttonLEDsColors(RED_RGB, GREEN_RGB, GREEN_RGB);
+
+    if (selectButtonPressed)
+    {
+      stepper->stopMove();
+      transitionToState(READY_TO_INJECT);
+    }
+
+    if (upButtonPressed && downButtonPressed)
+    {
+      transitionToState(InjectorStates::INJECT);
+    }
+
+    break;
+
+  case InjectorStates::INJECT:
+    buttonLEDsColors(RED_RGB, GREEN_RGB, BLACK_RGB); // middle/UP LED GREEN flashing
+
+    if (selectButtonPressed)
+    {
+      stepper->stopMove();
+      Serial.println("Motor moved " && stepper->getCurrentPosition() && " steps"); // send via serial the actual steps moved by motor: in the case mould was overfilling, can be useful info for user
+      //
+      transitionToState(InjectorStates::RELEASE);
+    }
+
+    break;
+
+  case InjectorStates::HOLD_INJECTION:
+    buttonLEDsColors(RED_RGB, GREEN_RGB, GREEN_RGB); // bottom/DOWN LED GREEN flashing
+
+    if (selectButtonPressed)
+    {
+      stepper->stopMove();
+      transitionToState(InjectorStates::RELEASE);
+    }
+
+    break;
+
+  case InjectorStates::RELEASE:
+    buttonLEDsColors(GREEN_RGB, GREEN_RGB, GREEN_RGB);
+    break;
+
+  case InjectorStates::CONFIRM_MOULD_REMOVAL:
+    buttonLEDsColors(GREEN_RGB, GREEN_RGB, GREEN_RGB);
+
+    if (endOfDayFlag == 0)
+    {
+      if (selectButtonPressed || upButtonPressed || downButtonPressed)
+      {
+        transitionToState(InjectorStates::REFILL);
+      }
+    }
+
+    if (endOfDayFlag == 1)
+    {
+      if (selectButtonPressed || upButtonPressed || downButtonPressed)
+      {
+        transitionToState(InjectorStates::READY_TO_INJECT);
+      }
+    }
+    break;
+  }
+}
+
+
+void stateMachineLoop() {
+  error = sanityCheck();
+  if (error != InjectorError::NO_ERROR) {
+      transitionToState(InjectorStates::ERROR_STATE);
+
+      Serial.printf("Found error!. Changing to ERROR_STATE to INIT_HEATING with error %d\n", error);
+  } 
+  // latch inputs, here you can simulate inputs
+  
+    // eval states
+  
+    // update outputs
+}
---
+++ ESP32_sketches/PlatformIO sketches/PP-motorized-injector/src/main.cpp
@@ -0,0 +1,335 @@
+
+/* program to control motorized injection machine via ESP32 for direct Nema34 160mm with external DM860H
+driver, 3 inductive endstops, 3 lower keypad buttons with Neopixel LEDs behind them to indicate distinct
+funtionality, SPI thermocouple, & independant Encoder to Motor comparison, all via a State Machine to 
+progress thru different machine states, using either sensor or user input via buttons
+
+sketch should return via Serial data on Encoder and Temp, later this ESP32 will be connected via UART to 
+a ESP32 Display, which will hold different mould structs, and therefore be able to load to the motor ESP32
+distinct injection parameters as per mould to inject, and also keep track from Encoder data of when the 
+injector is Refilled, so each Refill will be graphically represented by a rectangule inside a tube like 
+graphic, move down and change color as injection and refills happens, & have a timing tracker to be able 
+to see if it has had enough time to melt (avoiding cold injections!) */
+
+#include <Arduino.h>
+#include <SPI.h>
+
+#include <FastAccelStepper.h>
+#include <Adafruit_MAX31855.h> // temp board, was enclosed with quotes "" in example, not sure why
+#include <Adafruit_NeoPixel.h> // ws2812B RGB addressable LEDs
+#include <Bounce2.h>           // bounce library allows to time button presses
+#include <ESP32Encoder.h>
+
+#include "config.h"
+#include "injector_fsm.h"
+
+
+////////////////////////////////
+// Stepper
+////////////////////////////////
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepper = NULL;
+
+////////////////////////////////
+// Encoder
+////////////////////////////////
+ESP32Encoder encoder;
+
+Adafruit_MAX31855 thermocouple(TEMPNozzleVSPI_SCK_CLK, TEMPNozzleVSPI_Dpin_MOSI_CS, TEMPNozzleVSPI_MISO_DO);
+
+Adafruit_NeoPixel keypadleds = Adafruit_NeoPixel(keypadLedCount, WS2812B_BUTTON_LEDS_PIN);
+Adafruit_NeoPixel ringleds = Adafruit_NeoPixel(ringLedCount, WS2812B_RING_LEDS_PIN);
+
+////////////////////////////////
+// Input block
+////////////////////////////////
+
+Bounce2::Button downButton = Bounce2::Button();
+Bounce2::Button upButton = Bounce2::Button();
+Bounce2::Button selectButton = Bounce2::Button();
+Bounce2::Button topEndstop = Bounce2::Button();
+Bounce2::Button bottomEndstop = Bounce2::Button();
+Bounce2::Button barrelEndstop = Bounce2::Button();
+Bounce2::Button EMERGENCYstop = Bounce2::Button();
+
+/**
+ * 
+ */
+void bounceButtonsSetup() {
+  // IF YOUR BUTTON HAS AN INTERNAL PULL-UP or PULL-DOWN
+  downButton.attach(BUTTON_DOWN_PIN, INPUT_PULLUP);
+  upButton.attach(BUTTON_UP_PIN, INPUT_PULLUP);  // USE INTERNAL PULL-UP
+  selectButton.attach(BUTTON_SELECT_PIN, INPUT_PULLUP);
+  topEndstop.attach(ENDSTOP_TOP_PLUNGER_PIN, INPUT_PULLUP);
+  bottomEndstop.attach(ENDSTOP_BOTTOM_PLUNGER_PIN, INPUT_PULLUP);    //Top & Bottom NO, 5v
+  barrelEndstop.attach(ENDSTOP_BARREL_PLUNGER_PIN, INPUT_PULLDOWN);  //FINDA sensor is NC, 0v
+  EMERGENCYstop.attach(EMERGENCY_STOP_PIN, INPUT_PULLUP);    // CHECK THAT IS NO!!
+
+  // DEBOUNCE INTERVAL IN MILLISECONDS
+  downButton.interval(debounceInterval);
+  upButton.interval(debounceInterval);
+  selectButton.interval(debounceInterval);
+  topEndstop.interval(debounceInterval);
+  bottomEndstop.interval(debounceInterval);
+  barrelEndstop.interval(debounceInterval);
+  EMERGENCYstop.interval(debounceInterval);
+
+  // INDICATE THAT THE LOW STATE CORRESPONDS TO PHYSICALLY PRESSING THE BUTTON
+  downButton.setPressedState(LOW);
+  upButton.setPressedState(LOW);
+  selectButton.setPressedState(LOW);
+  topEndstop.setPressedState(LOW);  // NO, with pullup, goes LOW when closed
+  bottomEndstop.setPressedState(LOW);
+  barrelEndstop.setPressedState(HIGH);  //  NC, with pulldown, goes HIGH when active
+  EMERGENCYstop.setPressedState(LOW);   //  CHECK THAT IS NO!!
+}
+
+
+
+
+/**
+ * 
+ */
+void getInputs() {
+  if (readNozzleTemp) {
+    nozzleTemperature = thermocouple.readCelsius();
+  } 
+
+  if (readEmergencyStop) {
+    emergencyStop = EMERGENCYstop.pressed();
+  }
+
+  if (readSelectButton) {
+    selectButton.update();
+    selectButtonPressed = selectButton.pressed();
+  }
+
+  if (readUpButton) {
+    upButton.update();
+    upButtonPressed = upButton.pressed();
+  }
+
+  if (readDownButton) {
+    downButton.update();
+    downButtonPressed = downButton.pressed();
+  }
+
+  if (readTopEndStop) {
+    topEndstop.update();
+    topEndStopActivated = topEndstop.isPressed();
+  }
+
+  if (readBottomEndStop) {
+    bottomEndstop.update();
+    bottomEndStopActivated = bottomEndstop.isPressed();
+  }
+
+  if (readBarrelEndStop) {
+    barrelEndstop.update();
+    barrelEndStopActivated = barrelEndstop.isPressed();
+  }
+
+  actualENPosition = encoder.getCount() / 2;
+}
+
+
+////////////////////////////////
+// END Input block
+////////////////////////////////
+void clearLEDs()
+{
+  for (int i = 0; i < keypadLedCount; i++)
+  {
+    keypadleds.setPixelColor(i, 0);
+  }
+}
+
+boolean changeSelectLEDcolour;
+boolean changeUpLEDcolour;
+boolean changeDownLEDcolour;
+
+uint32_t currentSelectLEDcolour;
+uint32_t currentUpLEDcolour;
+uint32_t currentDownLEDcolour;
+
+/**
+ *
+ */
+void buttonLEDsColors(uint32_t newSelectLEDcolour, uint32_t newUpLEDcolour, uint32_t newDownLEDcolour) // could use case/switch instead? but are different types of defining behaviours..?
+{
+  changeSelectLEDcolour = currentSelectLEDcolour != newSelectLEDcolour;
+  if (changeSelectLEDcolour)
+  {
+    currentSelectLEDcolour = newSelectLEDcolour;
+  }
+
+  changeUpLEDcolour = currentUpLEDcolour != newUpLEDcolour;
+  if (changeUpLEDcolour)
+  {
+    currentUpLEDcolour = newUpLEDcolour;
+  }
+
+  changeDownLEDcolour = currentDownLEDcolour != newDownLEDcolour;
+  if (changeDownLEDcolour)
+  {
+    currentDownLEDcolour = newDownLEDcolour;
+  }
+}
+
+/**
+ *
+ */
+void outputButtonLEDsColors() // could use case/switch instead? but are different types of defining behaviours..?
+{
+  if (selectButtonPressed || upButtonPressed || downButtonPressed) // set brightness to about 80% on any button press
+  {
+    keypadleds.setBrightness(ledHBrightness);
+  }
+  else
+  {
+    keypadleds.setBrightness(ledLBrightness);
+  }
+
+  if (changeSelectLEDcolour)
+  {
+    keypadleds.setPixelColor(0, currentSelectLEDcolour);
+    changeSelectLEDcolour = false;
+  }
+
+  if (changeUpLEDcolour)
+  {
+    keypadleds.setPixelColor(1, currentUpLEDcolour);
+    changeUpLEDcolour = false;
+  }
+
+  if (changeDownLEDcolour)
+  {
+    keypadleds.setPixelColor(2, currentDownLEDcolour);
+    changeDownLEDcolour = false;
+  }
+
+  keypadleds.show();
+
+  return;
+}
+
+
+void setup() {
+    Serial.begin(9600);
+
+    delay(2000); // wait 2 seconds. REQUIERED !!!
+    Serial.println("PP_injector_ESP32");
+
+    // encoder setup
+
+// FIXME commented out for testing
+    //encoder.attachHalfQuad(ENCODER_A_PIN, ENCODER_B_PIN);  // possible have to reverse..? or rename, for Encoder library "CLK, DT ", and also rename EncoderPins..?
+    // encoder.setCount ( 0 );  move this line to INIT_HOMED_ENCODER_ZEROED function?
+
+    // stepper setup
+    //#define DRIVER_RMT 1  // type of driver for FAS, to separate any motor interference, not needed as directly used "1" below..?
+    engine.init();                                           
+    
+    // FIXME  Core assignment
+    // engine.init(1); 
+    // stepper = engine.stepperConnectToPin(STEPPER_STEP_PIN, 1);  // "1" defines DRIVER_RMT, for encoder to use PCNT, not Stepper
+    // refere to https://github.com/gin66/FastAccelStepper/blob/master/extras/doc/FastAccelStepper_API.md
+    // for ways to CPU_CORE & DRIVER_TYPE, if this does not compile as-is, and 
+    // https://valarsystems.com/blogs/val-2000/section-9-dual-core-setup for alternative CPU_CORE method
+
+    stepper = engine.stepperConnectToPin(STEPPER_STEP_PIN);
+    stepper->setAbsoluteSpeedLimit(8000);  // 16.000.000 tick/s / 2000steps/s maxSpeedLimit = 8000 ticks/ step
+                                                           // 16.000.000 tick/s / 5000steps/s maxSpeedLimit = 3200 ticks/ step
+
+    if (stepper) {
+      stepper->setDirectionPin(STEPPER_DIR_PIN);  // possible not needed if moveTo commands are also -ve..?
+    }
+
+    // thermocouple setup
+    thermocouple.begin();
+
+    // keypadLEDs setup
+    keypadleds.begin();  // Call this to start up the LED strip.
+    //  clearLEDs();   // This function, defined below, turns all LEDs off...
+    keypadleds.show();  // ...but the LEDs don't actually update until you call this.
+
+    // Buttons & Endstops Bounce2 setup is very long, so called from the following function
+    bounceButtonsSetup();
+
+    currentState = InjectorStates::INIT_HEATING;
+    error = InjectorError::NO_ERROR;
+
+    readNozzleTemp = true;
+    readEmergencyStop = true;
+}
+
+long now; 
+long fastTaskTime = 0;
+long mediumTaskTime = 0;
+long slowTaskTime = 2;
+
+
+/** 
+ * Comms functions, every 100ms send data via serial
+ */
+void serialRegular100msMessages() {
+  Serial.println("===============");
+  //Serial.printf("loops: %d, avg time: %d\n", numLoops, avgLoopTime);
+  Serial.printf("state: %d, ER: %d\n",currentState, error);
+  Serial.println("selectLed: " + String(currentSelectLEDcolour, HEX));
+  Serial.println("upLed: " + String(currentUpLEDcolour, HEX));
+  Serial.println("downLed: " + String(currentDownLEDcolour, HEX));
+
+  Serial.printf("temps: NZ=%d\n", nozzleTemperature);
+
+  Serial.printf("inputs: ES=%d, SEL=%d, UP=%d, DW=%d, TE=%d, BE=%d, BR=%d\n", emergencyStop, selectButtonPressed, upButtonPressed, downButtonPressed, topEndStopActivated, bottomEndStopActivated, barrelEndStopActivated);
+
+// // FIXME commented out for testing
+//       //actualENPosition = encoder.getCount() / 2;
+//       if (actualENPosition != oldENPosition) /* ... and actualENPosition & oldENPosition are updated by EncoderActualPosition, but if there
+//      there has been no change in last 100ms, then do not print new position... possibly this should be divided in loop..? */
+//       {
+//         Serial.print("Encoder: ");
+//         Serial.println(actualENPosition);
+//         oldENPosition = actualENPosition;
+//       }
+//       Serial.print("TempBox: ");
+//       Serial.println(thermocouple.readInternal());
+//       Serial.print("TempNozzle: ");
+//       Serial.println(nozzleTemperature);
+
+}
+
+void loop() {
+  // this eventloop runs tasks based on 3 different rates: high speed tasks running every loop interation, fast tasks running every 1ms, medium tasks at 10ms, and slow tasks at 100ms
+  
+  // high speed tasks
+  downButton.update();
+  upButton.update();
+  selectButton.update();
+  topEndstop.update();
+  bottomEndstop.update();
+  barrelEndstop.update();
+  EMERGENCYstop.update();
+  
+
+  now = millis();
+  if (now - fastTaskTime  >= 1) {
+    fastTaskTime = now;
+    // fast tasks
+  
+  }
+  if (now - mediumTaskTime >= 10) {
+    mediumTaskTime = now;
+    // medium tasks
+    //stateMachineLoop();
+
+  }
+  if (now - slowTaskTime >= 100) {
+    slowTaskTime = now;
+    // slow tasks
+    serialRegular100msMessages();
+  }
+
+}
+
